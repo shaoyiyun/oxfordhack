ae73c3fe58838aa05df8d8f328ef332f294991c0
ZihanYe <cloverlemo@hotmail.com>
2017-11-26 00:42:00 +0000

    with audio

--- msft.py
+++ msft.py
@@ -2,17 +2,31 @@ import requests
 
 import audio
 
+colours = {
+    'green' : [0,150,0],
+    'yellow' : [250,155,0],
+    'blue' : [0,0,150],
+    'red' : [150,0,0]
+}
+
 def speechToText():
 
-	url = 'https://speech.platform.bing.com/speech/recognition/interactive/cognitiveservices/v1?language=en-GB&format=simple'
+    url = 'https://speech.platform.bing.com/speech/recognition/interactive/cognitiveservices/v1?language=en-GB&format=simple'
+	
+    data = open(audio.record(), 'rb').read()
 
-	data = open(audio.record(), 'rb').read()
+    headers = {
+        'Ocp-Apim-Subscription-Key': 'e5f16159f94c40f78cc5e720421ea231',
+        'Content-type': 'audio/wav; codec=audio/pcm; samplerate=16000'
+    }
 
-	headers = {
-		'Ocp-Apim-Subscription-Key': 'e5f16159f94c40f78cc5e720421ea231',
-		'Content-type': 'audio/wav; codec=audio/pcm; samplerate=16000'
-	}
+    res = requests.post(url=url, data=data, headers=headers)
+    return getTheColor(res['DisplayText'])
+    #return res.json()
 
-	res = requests.post(url=url, data=data, headers=headers)
+def getTheColor(res):
+    for key in colours:
+        if key in res:
+            return colours[key]
+    return [256,0,0]
 
-	return res.json()
